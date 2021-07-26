6466775b5f5371523a06a884b204f5e2316ed423
Anwarkamal143 <anwarkamal1434@gmail.com>
2021-07-27 00:18:02 +0500

    load from npmreg

--- src/Pages/ChatWidget/ChatWidget.tsx
+++ src/Pages/ChatWidget/ChatWidget.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react'
-import { Chat } from '../../components/Chat'
+// import { Chat } from '../../components/Chat'
 import styled from 'styled-components'
 
 
